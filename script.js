fad2b65928bced2b32b75104c17d1faa11af6b8a
Carl Portuguese <itsukikai078@gmail.com>
2026-08-10 21:53:26 +0800

    mhmm3

--- script.js
+++ script.js
@@ -40,10 +40,10 @@ const playlist = [
   { src: 'audio/IV OF SPADES - Kabisado.mp3', title: 'Kabisado', artist: 'IV OF SPADES', img: 'imgs/mc3.png' },
   { src: 'audio/Los Retros - Someone To Spend Time With.mp3', title: 'Someone To Spend Time With', artist: 'Los Retros', img: 'imgs/mc1.png' },
   { src: 'audio/Mac DeMarco - My Kind Of Woman .mp3', title: 'My Kind Of Woman', artist: 'Mac DeMarco', img: 'imgs/mc2.png' },
+  { src: 'audio/WALTZ OF FOUR LEFT FEET- SHIREBOUND AND BUSKING.mp3', title: 'Waltz of Four Left Feet', artist: 'shirebound', img: 'imgs/mc1.png' },
   { src: 'audio/Antukin - Rico Blanco.mp3', title: 'Antukin', artist: 'Rico Blanco', img: 'imgs/mc1.png' },
   { src: 'audio/Orange & Lemons - Yakap Sa Dilim.mp3', title: 'Yakap Sa Dilim', artist: 'Orange & Lemons', img: 'imgs/mc2.png' },
   { src: 'audio/The 1975 - About You.mp3', title: 'About You', artist: 'The 1975', img: 'imgs/mc3.png' },
-  { src: 'audio/WALTZ OF FOUR LEFT FEET- SHIREBOUND AND BUSKING.mp3', title: 'Waltz of Four Left Feet', artist: 'shirebound', img: 'imgs/mc1.png' },
   { src: 'audio/pahintulot - shirebound.mp3', title: 'Pahintulot', artist: 'shirebound', img: 'imgs/mc2.png' },
   { src: 'audio/wave to earth - seasons.mp3', title: 'seasons', artist: 'wave to earth', img: 'imgs/mc3.png' }
 ];
